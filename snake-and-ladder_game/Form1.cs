c73364ae1b35ff8bd2c9e0f777c24c9a1a4fe181
Mahmoud ElHassan <mahmoudelhassan9@gmail.com>
2022-12-07 18:34:14 +0200

    Add project files.

---
+++ snake-and-ladder_game/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_and_ladder_game
+{
+    public partial class Form1 : Form
+    {
+        int flag = 0;
+        bool blue = false ,red = false;
+        int x = 3, y = 492, diceValue, p=0;
+        int bx = 39, by = 492, q = 0;
+
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                button2.Enabled = false;
+            }
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox3.Image = Image.FromFile(@"D:\_Projects\snake-and-ladder_game\snake-and-ladder_game\Resources\logo.jpeg");
+            pictureBox3.SizeMode=PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            diceValue = logics.rolldice(pictureBox3);
+            label8.Text = diceValue.ToString();
+
+            if (blue == true)
+            {
+                p = logics.move(ref x, ref y, ref p, diceValue, label9 , pictureBox4);
+                label9.Text = p.ToString();
+            }
+
+            if (label8.Text == "6" && blue == false)
+            {
+                pictureBox4.Visible = true;
+                pictureBox1.Visible = false;
+                blue = true;
+                pictureBox4.Location = new Point(x,y);
+                label4.Text = x.ToString();
+                label6.Text = y.ToString();
+                label9.Text = p.ToString();
+                p++;
+            }
+
+            if (p == 101)
+            {
+                MessageBox.Show("WINNER");
+                button1.Enabled = false;
+            }
+            p = logics.snake(ref x, ref y, p, pictureBox4);
+            p = logics.ladder(ref x, ref y, p, pictureBox4);
+            label9.Text = p.ToString();
+
+            if (diceValue == 6)
+            {
+                flag = 0;
+            }
+            else
+            {
+                flag = 1;
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            diceValue = logics.rolldice(pictureBox3);
+            label8.Text = diceValue.ToString();
+
+            if (red == true)
+            {
+                q = logics.move(ref bx, ref by, ref q, diceValue, label11, pictureBox5);
+                label11.Text = q.ToString();
+            }
+
+            if (label8.Text == "6" && red == false)
+            {
+                pictureBox5.Visible = true;
+                pictureBox2.Visible = false;
+                red = true;
+                pictureBox5.Location = new Point(x, y);
+                label4.Text = bx.ToString();
+                label6.Text = by.ToString();
+                label11.Text = q.ToString();
+                q++;
+            }
+
+            if (q == 101)
+            {
+                MessageBox.Show("WINNER");
+                button2.Enabled = false;
+            }
+            q = logics.snake(ref bx, ref by, q, pictureBox5);
+            q = logics.ladder(ref bx, ref by, q, pictureBox5);
+            label11.Text = q.ToString();
+
+            if (diceValue == 6)
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ snake-and-ladder_game/logics.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_and_ladder_game
+{
+    internal class logics
+    {
+        public static int rolldice(PictureBox px)
+        {
+            int dice = 0;
+            Random r = new Random();
+            dice = r.Next(1, 7);
+            px.Image = Image.FromFile(@"D:\_Projects\snake-and-ladder_game\snake-and-ladder_game\Resources\" + dice +".png");
+            px.SizeMode = PictureBoxSizeMode.StretchImage;
+            return dice;
+        }
+
+        public static int move(ref int x, ref int y, ref int p, int dice,Label l,PictureBox px)
+        {
+            if (dice + p > 101)
+            {
+                l.Text = "Can't Move";
+                l.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                for (int i = 0; i < dice; i++)
+                {
+                    if (p == 10)
+                    {
+                        x = 3;
+                        y = 439;
+                    }
+                    else if (p == 20)
+                    {
+                        x = 3;
+                        y = 386;
+                    }
+                    else if (p == 30)
+                    {
+                        x = 3;
+                        y = 333;
+                    }
+                    else if (p == 40)
+                    {
+                        x = 3;
+                        y = 280;
+                    }
+                    else if (p == 50)
+                    {
+                        x = 3;
+                        y = 220;
+                    }
+                    else if (p == 60)
+                    {
+                        x = 3;
+                        y = 174;
+                    }
+                    else if (p == 70)
+                    {
+                        x = 3;
+                        y = 127;
+                    }
+                    else if (p == 80)
+                    {
+                        x = 3;
+                        y = 75;
+                    }
+                    else if (p == 90)
+                    {
+                        x = 3;
+                        y = 15;
+                    }
+                    else
+                    {
+                        x += 72;
+                        l.Text = p.ToString();
+                    }
+
+                    l.Text = p.ToString();
+                    px.Location = new Point(x, y);
+                    p++;
+                }
+            }
+            return p;
+        }
+
+        public static int snake(ref int x,ref int y,int p,PictureBox px)
+        {
+            if (p == 25)
+            {
+                x = 291;
+                y = 491;
+                p = 5;
+                px.Location = new Point(x, y);
+            }
+            else if (p == 34)
+            {
+                x = 3;
+                y = 492;
+                p = 1;
+            }
+            else if (p == 47)
+            {
+                x = 577;
+                y = 491;
+                p = 9;
+            }
+            else if (p == 65)
+            {
+                x = 78;
+                y = 227;
+                p = 52;
+            }
+            else if (p == 87)
+            {
+                x = 435;
+                y = 227;
+                p = 57;
+            }
+            else if (p == 91)
+            {
+                x = 3;
+                y = 180;
+                p = 61;
+            }
+            return p;
+        }
+
+        public static int ladder(ref int x, ref int y, int p, PictureBox px)
+        {
+            if (p == 3)
+            {
+                x = 3;
+                y = 227;
+                p = 51;
+            }
+            else if (p == 6)
+            {
+                x = 433;
+                y = 386;
+                p = 27;
+            }
+            else if (p == 20)
+            {
+                x = 648;
+                y = 180;
+                p = 70;
+            }
+            else if (p == 36)
+            {
+                x = 291;
+                y = 227;
+                p = 55;
+            }
+            else if (p == 63)
+            {
+                x = 291;
+                y = 22;
+                p = 95;
+            }
+            else if (p == 68)
+            {
+                x = 506;
+                y = 22;
+                p = 99;
+            }
+            return p;
+        }
+    }
+}
